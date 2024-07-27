9534074fd4f7f3620d18462fd659f81329684fb2
cqw-acq <133117051+cqw-acq@users.noreply.github.com>
2024-07-27 11:33:01 +0800

    Update K_kill.java

--- src/main/java/org/example/helloworld/k_kill/K_kill.java
+++ src/main/java/org/example/helloworld/k_kill/K_kill.java
@@ -1,13 +1,18 @@
 package org.example.helloworld.k_kill;
-
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class K_kill extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // 在插件启用时注册命令
+        this.getCommand("k").setExecutor(new SuicideCommand());
     }
 
     @Override
